2b0e59860fdac84618cfaa62b17f6a24b2df3b97
Moosa Azfar <moosa.azfar@gmail.com>
2024-06-17 23:04:00 -0700

    Initial commit

---
+++ build/CMakeFiles/DAW.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DAW.dir/main.cpp.obj"
+  "DAW.pdb"
+  "DAW.exe"
+  "DAW.exe.manifest"
+  "libDAW.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DAW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
